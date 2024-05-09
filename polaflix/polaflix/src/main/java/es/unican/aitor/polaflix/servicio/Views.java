e63980b77a84ae4651590a70cafdad061bc6be4b
AitorJuncalUC <144138499+AitorJuncalUC@users.noreply.github.com>
2024-05-10 00:24:35 +0200

    d

--- polaflix/polaflix/src/main/java/es/unican/aitor/polaflix/servicio/Views.java
+++
@@ -1,8 +0,0 @@
-package es.unican.aitor.polaflix.servicio;
-
-public class Views {
-	public static class UsuarioView {};
-	public static class SerieView {};
-	public static class FacturaView {};
-	public static class CapituloVistoView {};
-}
