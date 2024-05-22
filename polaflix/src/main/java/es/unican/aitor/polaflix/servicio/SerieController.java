fdcc0119de23c140b293247f35854e16e42de80c
AitorJuncalUC <144138499+AitorJuncalUC@users.noreply.github.com>
2024-05-22 13:25:44 +0200

    cambio formato fecha

--- polaflix/src/main/java/es/unican/aitor/polaflix/servicio/SerieController.java
+++ polaflix/src/main/java/es/unican/aitor/polaflix/servicio/SerieController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import es.unican.aitor.polaflix.dominio.Serie;
 
 
 @RestController
+@CrossOrigin(origins= "http://localhost:4200", maxAge=3600)
 @RequestMapping("/series")
 public class SerieController {
 	@Autowired
--- polaflix/src/main/java/es/unican/aitor/polaflix/servicio/UsuarioController.java
+++ polaflix/src/main/java/es/unican/aitor/polaflix/servicio/UsuarioController.java
@@ -9,6 +9,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -26,6 +27,7 @@ import jakarta.transaction.Transactional;
 
 
 @RestController
+@CrossOrigin(origins= "http://localhost:4200", maxAge=3600)
 @RequestMapping("/usuarios")
 public class UsuarioController {
 	
@@ -60,7 +62,7 @@ public class UsuarioController {
         	facturas = us.getFacturas(nombre);
         }
         else {
-        	SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        	SimpleDateFormat dateFormat = new SimpleDateFormat("MM-yyyy");
     		Date date = null;
     		try {
     			date = dateFormat.parse(fecha);
